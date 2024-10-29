9ab32db2acbd1457ebf12307093cecdcc443cdbd
Kaijar2020 <kizar.cse@gmail.com>
2024-10-29 18:24:21 +0600

    svg element added

--- src/test/java/webelementshandle/SVGElementHandle.java
+++ src/test/java/webelementshandle/SVGElementHandle.java
@@ -1,15 +1,29 @@
 package webelementshandle;
 
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 
 public class SVGElementHandle {
 
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://testautomationpractice.blogspot.com");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         driver.manage().window().maximize();
+
+        driver.findElement(By.xpath("//input[@placeholder='Username']")).sendKeys("Admin");
+        driver.findElement(By.xpath("//input[@placeholder='Password']")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+
+        //Identify SVG element.
+//        driver.findElement(By.xpath("//a[@class='oxd-main-menu-item active']//*[name()='svg']")).click();
+        driver.findElement(By.xpath("//button[@title='Timesheets']//*[name()='svg']")).click();
+        //driver.close();
     }
-
-
 }
